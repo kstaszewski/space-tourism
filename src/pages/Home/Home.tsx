8fd34a71e1da4ea66181e56066d0905962cdf093
Krzysztof Staszewski <ks.staszewski@gmail.com>
2022-10-07 14:13:36 +0200

    cd

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import css from './Home.module.css';
 
-type Props = {};
-
-function Home({ }: Props) {
+function Home() {
   return (
     <>
       <div className={css.container}>
-
+        <p>SO,YOU WANT TO TRAVEL TO</p>
+        <h1>SPACE</h1>
+        <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
       </div>
     </>
   );
